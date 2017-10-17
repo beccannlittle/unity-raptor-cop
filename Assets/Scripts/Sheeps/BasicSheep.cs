14789fe9ab357e0321b79533dc31fc1d72295562
David Nadeau <nadeaud.d@gmail.com>
2017-10-16 19:51:55 -0700

    Beginnings of a state machine and some dorky animations

---
+++ Assets/Scripts/Sheeps/BasicSheep.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSheep : MonoBehaviour {
+	public float hunger = 50.0f;
+	public float health = 15.0f;
+	private float hungerDecayRate = 10.0f;
+	public float hungerThresholdMin = 30.0f;
+	public float hungerThresholdMax = 100.0f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (HungerDecay());	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (hunger <= hungerThresholdMin) {
+			Debug.Log ("TOO HUNGRY MUST EAT!");
+			gameObject.GetComponent<SheepStateController> ().state = SheepStateController.State.Graze;
+		} else if (hunger >= hungerThresholdMax) {
+			Debug.Log ("Ahhhh nice and full!");
+			gameObject.GetComponent<SheepStateController> ().state = SheepStateController.State.Idle;
+		}
+
+	}
+
+	IEnumerator HungerDecay(){
+		while(health > 0.0f && hunger > 0.0f){
+			float oldHunger = hunger;
+			hunger = oldHunger - hungerDecayRate;
+			Debug.Log ("HungerDecays from ["+oldHunger+"] to ["+hunger+"]");
+			yield return new WaitForSeconds(1.0f);
+		}
+		Debug.Log ("Death by Starvation");
+	}
+
+}
---
+++ Assets/Scripts/Sheeps/SheepStateController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepStateController : MonoBehaviour {
+	public enum State {
+		Idle,
+		Graze,
+		Startled,
+		Petrified,
+		Dead
+	}
+	public State state;
+
+	IEnumerator IdleState(){
+		while(state.Equals(State.Idle)){
+			ChangeColor (Color.white);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+	IEnumerator GrazeState(){
+		while(state.Equals(State.Graze)){
+			ChangeColor (Color.green);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+	IEnumerator StartledState(){
+		while(state.Equals(State.Startled)){
+			ChangeColor (Color.yellow);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+	IEnumerator PetrifiedState(){
+		while(state.Equals(State.Petrified)){
+			ChangeColor (Color.grey);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+	IEnumerator DiedState(){
+		while(state.Equals(State.Dead)){
+			ChangeColor (Color.black);
+			yield return new WaitForSeconds(2.0f);
+		}
+	}
+
+
+	void Start () {
+		state = State.Idle;
+		StartCoroutine (IdleState ());
+	}
+
+	void ChangeColor(Color newcolor){
+		//Debug.Log ("I am being Idle");
+		foreach (Renderer childRenderer in GetComponentsInChildren<Renderer>()) {
+			childRenderer.material.color = newcolor;
+			//Debug.Log ("Found Child Object: "+childRenderer.gameObject.name);
+		}
+	}
+
+}
