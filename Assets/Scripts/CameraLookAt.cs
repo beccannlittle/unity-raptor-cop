f14b0cce58cf3be78f4093e38397ef3d092b1671
Becca Little <beccannlittle@gmail.com>
2017-10-18 18:27:59 -0700

    raptor can move somewhat with the camera following

---
+++ Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour {
+
+	public GameObject target;
+	public float followDistance;
+	public float rotAngle;
+	public float downAngle;
+
+	InputManager inputManager;
+
+	private void Awake() {
+		inputManager = target.GetComponent<InputManager>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		rotAngle = inputManager.CurrentRotation;
+		Quaternion rotation = Quaternion.AngleAxis (rotAngle, Vector3.up) * Quaternion.AngleAxis (downAngle, Vector3.right);
+		Vector3 direction = rotation * Vector3.forward;
+		transform.position = target.transform.position + (direction.normalized*followDistance);
+		transform.LookAt (target.transform);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float playerSpeed;
+
+	InputManager inputManager;
+
+	private void Awake() {
+		inputManager = GetComponent<InputManager>();
+	}
+
+	void Update () {
+		transform.Translate(inputManager.CurrentMovement * Time.deltaTime * playerSpeed);
+		Vector3 tempRot = transform.rotation.eulerAngles;
+		tempRot.y += inputManager.CurrentRotation * Time.deltaTime;
+		transform.rotation = new Quaternion (tempRot);
+	}
+}
