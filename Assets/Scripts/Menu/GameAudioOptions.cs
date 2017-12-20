93481b1a0c301a203e7e96afb97ff461b3757d3d
David Nadeau <nadeaud.d@gmail.com>
2017-12-19 17:04:30 -0700

    Added Options Saving logic. Still need to update UI and integrate it with the GameAudioOptions

---
+++ Assets/Scripts/Menu/GameAudioOptions.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameAudioOptions : MonoBehaviour {
+
+	public static GameAudioOptions control;
+	[Header("Audio Option Data")]
+	public bool masterVolumeEnabled;
+	public bool sfxVolumeEnabled;
+	public bool uiVolumeEnabled;
+	public bool ambientVolumeEnabled;
+	public float masterVolume;
+	public float sfxVolume;
+	public float uiVolume;
+	public float ambientVolume;
+
+	// Use this for initialization
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if(!control.Equals(this)) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.Label (new Rect (10, 10, 100, 30), "Health: " + masterVolumeEnabled);
+	}
+
+	public void SaveAudioOptions(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/gameAudioOptions.dat");
+
+		GameAudioOptionsData optionsData = new GameAudioOptionsData ();
+		optionsData.masterVolumeEnabled = masterVolumeEnabled;
+		optionsData.sfxVolumeEnabled = sfxVolumeEnabled;
+		optionsData.uiVolumeEnabled = uiVolumeEnabled;
+		optionsData.ambientVolumeEnabled = ambientVolumeEnabled;
+		optionsData.masterVolume = masterVolume;
+		optionsData.sfxVolume = sfxVolume;
+		optionsData.uiVolume = uiVolume;
+		optionsData.ambientVolume = ambientVolume;
+
+		bf.Serialize (file, optionsData);
+		file.Close ();
+	}
+
+	public void LoadAudioOptions(){
+		if(File.Exists(Application.persistentDataPath + "/gameAudioOptions.dat")){
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/gameAudioOptions.dat", FileMode.Open);			
+			GameAudioOptionsData data = (GameAudioOptionsData)bf.Deserialize (file);
+			file.Close ();
+
+			masterVolumeEnabled = data.masterVolumeEnabled;
+			sfxVolumeEnabled = data.sfxVolumeEnabled;
+			uiVolumeEnabled = data.uiVolumeEnabled;
+			ambientVolumeEnabled = data.ambientVolumeEnabled;
+			masterVolume = data.masterVolume;
+			sfxVolume = data.sfxVolume;
+			uiVolume = data.uiVolume;
+			ambientVolume = data.ambientVolume;
+		}
+	}
+}
+
+[Serializable]
+class GameAudioOptionsData {
+
+	[Header("Audio Option Data")]
+	public bool masterVolumeEnabled;
+	public bool sfxVolumeEnabled;
+	public bool uiVolumeEnabled;
+	public bool ambientVolumeEnabled;
+	public float masterVolume;
+	public float sfxVolume;
+	public float uiVolume;
+	public float ambientVolume;
+
+
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+class PlayerData {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
