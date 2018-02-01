181d0d42f8b73c63c8a4d0cc16241e44e2132ee7
Becca Little <beccannlittle@gmail.com>
2018-01-31 22:34:39 -0700

    refactored MenuActions. having an issue getting Options to work

--- Assets/Scripts/Menu/MenuActions.cs
+++ Assets/Scripts/Menu/MenuActions.cs
@@ -1,20 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour {
+
+	public GameObject gameManager;
 	public GameObject escapeMenu;
 	public GameObject optionsMenu;
 
+	void Awake() {
+		// TODO: Audio options don't work
+//		gameManager.GetComponent<GameAudioOptions> ().LoadAudioOptions ();
+	}
+
 	void Update () {
-		if(!optionsMenu.activeSelf && PressedEscape){
-			escapeMenu.GetComponent<EscapeMenu>().ToggleEscapeMenuVisibility ();
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if (escapeMenu.activeSelf) {
+				Resume ();
+			} else if (optionsMenu.activeSelf) {
+				ToggleOptionsMenu ();
+			} else {
+				Pause ();
+			}
 		}
 	}
 
-	public bool PressedEscape {
-		get {
-			return Input.GetKeyDown(KeyCode.Escape);
-		}
+	private void Pause() {
+		escapeMenu.SetActive (true);
 	}
+
+	public void Resume() {
+		escapeMenu.SetActive (false);
+	}
+
+	public void Save() {
+		gameManager.GetComponent<GameControl> ().ClearSaveData ();
+		gameManager.GetComponent<GameControl> ().SaveGameData ();
+		Resume ();
+	}
+
+	public void SaveAndQuit() {
+		Save ();
+		Quit ();
+	}
+
+	public void Quit() {
+		SceneManager.LoadScene (GameConstants.SCENE_TITLEMENU);	
+		Destroy (gameManager);
+	}
+
+	public void ToggleOptionsMenu() {
+		Debug.Log(escapeMenu.activeSelf);
+		escapeMenu.SetActive (!escapeMenu.activeSelf);
+		Debug.Log(escapeMenu.activeSelf);
+		Debug.Log(optionsMenu.activeSelf);
+		optionsMenu.SetActive (!optionsMenu.activeSelf);
+		Debug.Log(optionsMenu.activeSelf);
+	}
+
+//	public void SaveOptions(){
+//		Debug.Log ("[GameManager]: Saving the game");
+//		gameManager.GetComponent<GameAudioOptions> ().SaveAudioOptions ();
+//
+//	}
+//	public void ToggleMuteAll(bool value){
+//		if (value) {
+//			AudioListener.volume = 1f;
+//		} else {
+//			AudioListener.volume = 0f;
+//		}
+//	}
+
 }
