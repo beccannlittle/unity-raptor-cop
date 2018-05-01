ecce27a9a4288cf258388435813f9e1282d6f9ef
David Nadeau <nadeaud.d@gmail.com>
2018-04-30 20:59:59 -0700

    Refactored some more LevelController stuff.

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -7,6 +7,9 @@ using System.IO;
 using UnityEngine.SceneManagement;
 
 public class Level01Controller : LevelController {
+	//Level01Controller should manage what/when/how to save/load game data
+	//LevelController should provide common tools to save/load game data
+
 	public GameObject sheepPrefab;
 	public GameObject buildingPrefab;
 	public GameObject playerOBJ;
@@ -15,101 +18,24 @@ public class Level01Controller : LevelController {
 
 	public override LevelData GenerateLevelData(){
 		Debug.Log ("In the Level01 GenerateLevelData");
-		Level01Data levelData = new Level01Data ();
+		CombatLevelData levelData = new CombatLevelData ();
 		levelData.sceneName = SceneManager.GetActiveScene ().name;
 		levelData.score = scoreManager.getPlayerScore();
 		levelData.playerdata = playerOBJ.GetComponent<Player>().BuildPlayerData ();
-		levelData.sheepdata = BuildListSheepData ();
-		levelData.buildingdata = BuildListBuildingData ();
+		levelData.sheepdata = BuildListSheepData (sheepOBJHolder);
+		levelData.buildingdata = BuildListBuildingData (buildingOBJHolder);
 		Debug.Log ("Score" + scoreManager.getPlayerScore ());
 		Debug.Log ("Saving Score" + levelData.score);
 		return levelData;
 	}
 
 	public override void LoadLevelData(LevelData leveld) {
-		Level01Data data = (Level01Data)leveld;
-		ClearSceneData ();
+		//We should modify this so that it just moves/deletes the existing sheep/buildings in the scene instead of clear and instantiate new ones
+		CombatLevelData data = (CombatLevelData)leveld;
+		ClearCombatSceneData (sheepOBJHolder, buildingOBJHolder);
 		if (data != null) {
-			LoadGameControlData (data);
+			LoadCombatScene (data,sheepPrefab, buildingPrefab);
 		}
 		scoreManager.QuerySheepRemaining ();
 	}
-	public void ClearSceneData() {
-		if (sheepOBJHolder != null) {
-			foreach (Transform sheep in sheepOBJHolder.transform) {
-				sheep.tag = "Untagged";
-				Destroy (sheep.gameObject);
-			}
-		}
-		if (buildingOBJHolder != null) {
-			foreach (Transform building in buildingOBJHolder.transform) {
-				Destroy (building.gameObject);
-			}
-		}
-	}
-	public void LoadGameControlData(Level01Data leveldata){
-		Debug.Log ("Loading Game (JSON):" + JsonUtility.ToJson (leveldata));
-		if (leveldata != null) {
-			LoadSavedPlayer (leveldata.playerdata);
-			LoadListSheepData (leveldata.sheepdata);
-			LoadListBuildingData (leveldata.buildingdata);
-
-			scoreManager.setPlayerScore (leveldata.score);
-		}
-	}
-
-	private List<SheepData> BuildListSheepData(){
-		List<SheepData> sheepDataList = new List<SheepData>();
-		foreach(Transform sheep in sheepOBJHolder.transform){
-			SheepData sd = sheep.gameObject.GetComponent<SheepEnemy> ().BuildSheepData ();
-			sheepDataList.Add (sd);
-		}
-		return sheepDataList;
-	}
-
-	private List<BuildingData> BuildListBuildingData(){
-		List<BuildingData> buildingDataList = new List<BuildingData> ();
-		foreach(Transform building in buildingOBJHolder.transform){
-			BuildingData bd = building.gameObject.GetComponent<Building> ().BuildBuildingData ();
-
-			buildingDataList.Add (bd);
-		}
-		return buildingDataList;
-	}
-
-	private void LoadSavedPlayer(PlayerData pd){
-		if (pd != null) {
-			GameObject player = GameObject.FindGameObjectWithTag ("Player");
-			if (player != null) {
-				player.GetComponent<Player> ().LoadPlayerData (pd);
-			}
-		}
-	}
-
-	private void LoadListSheepData(List<SheepData> sheepdatalist){
-		GameObject enemyList = GameObject.FindGameObjectWithTag ("EnemyList");
-		if (enemyList != null && sheepdatalist != null) {
-			foreach (SheepData sd in sheepdatalist) {
-				GameObject newSheep = Instantiate (sheepPrefab, sd.getPositionVector3(), sd.getRotationQuaternion(), enemyList.transform);
-				newSheep.GetComponent<StateCartridgeController> ().state = sd.sheepstate;
-			}
-		}
-	}
-
-	private void LoadListBuildingData(List<BuildingData> buildingdatalist){
-		GameObject buildingList = GameObject.FindGameObjectWithTag ("BuildingList");
-		if(buildingList != null && buildingdatalist != null) {
-			foreach(BuildingData bd in buildingdatalist){
-				GameObject buildingOBJ = Instantiate (buildingPrefab, bd.getPositionVector3(), bd.getRotationQuaternion(), buildingList.transform);
-				buildingOBJ.GetComponent<Building> ().m_health.val = bd.health;
-			}
-		}
-	}
-}
-[Serializable]
-public class Level01Data : LevelData {
-	public PlayerData playerdata;
-	public List<SheepData> sheepdata;
-	public List<BuildingData> buildingdata;
-	
 }
--- Assets/Scripts/Utils/SaveGame/LevelController.cs
+++ Assets/Scripts/Utils/SaveGame/LevelController.cs
@@ -20,6 +20,88 @@ public abstract class LevelController : MonoBehaviour {
 	public abstract LevelData GenerateLevelData ();
 	public abstract void LoadLevelData (LevelData leveld);
 
+	public void ClearDataFromList (GameObject objectHolder){
+		if (objectHolder != null) {
+			foreach (Transform childObject in objectHolder.transform) {
+				childObject.tag = "Untagged";
+				Destroy (childObject.gameObject);
+			}
+		}
+	}
+	public List<SheepData> BuildListSheepData(GameObject sheepOBJHolder) {
+		List<SheepData> sheepDataList = new List<SheepData>();
+		foreach(Transform sheep in sheepOBJHolder.transform){
+			SheepData sd = sheep.gameObject.GetComponent<SheepEnemy> ().BuildSheepData ();
+			sheepDataList.Add (sd);
+		}
+		return sheepDataList;
+	}
+	public List<BuildingData> BuildListBuildingData(GameObject buildingOBJHolder){
+		List<BuildingData> buildingDataList = new List<BuildingData> ();
+		foreach(Transform building in buildingOBJHolder.transform){
+			BuildingData bd = building.gameObject.GetComponent<Building> ().BuildBuildingData ();
+
+			buildingDataList.Add (bd);
+		}
+		return buildingDataList;
+	}
+
+	public void LoadBonusLevel(LevelData leveldata,GameObject sheepPrefab, GameObject buildingPrefab){
+		//Instantiates new objects based on the loaded LevelData.
+		BonusLevelData thisLevelData = (BonusLevelData) leveldata;
+		Debug.Log ("Loading Game (JSON):" + JsonUtility.ToJson (thisLevelData));
+		if (thisLevelData != null) {
+			LoadSavedPlayer (thisLevelData.playerdata);
+			LoadListSheepData (thisLevelData.sheepdata, sheepPrefab);
+
+			scoreManager.setPlayerScore (thisLevelData.score);
+		}
+	}
+	public void LoadCombatScene(LevelData leveldata,GameObject sheepPrefab, GameObject buildingPrefab){
+		//Instantiates new objects based on the loaded LevelData.
+		CombatLevelData thisLevelData = (CombatLevelData) leveldata;
+		Debug.Log ("Loading Game (JSON):" + JsonUtility.ToJson (thisLevelData));
+		if (thisLevelData != null) {
+			LoadSavedPlayer (thisLevelData.playerdata);
+			LoadListSheepData (thisLevelData.sheepdata, sheepPrefab);
+			LoadListBuildingData (thisLevelData.buildingdata, buildingPrefab);
+
+			scoreManager.setPlayerScore (thisLevelData.score);
+		}
+	}
+	public void LoadSavedPlayer(PlayerData pd){
+		if (pd != null) {
+			GameObject player = GameObject.FindGameObjectWithTag ("Player");
+			if (player != null) {
+				player.GetComponent<Player> ().LoadPlayerData (pd);
+			}
+		}
+	}
+
+	public void LoadListSheepData(List<SheepData> sheepdatalist, GameObject prefab){
+		GameObject enemyList = GameObject.FindGameObjectWithTag ("EnemyList");
+		if (enemyList != null && sheepdatalist != null) {
+			foreach (SheepData sd in sheepdatalist) {
+				GameObject newSheep = Instantiate (prefab, sd.getPositionVector3(), sd.getRotationQuaternion(), enemyList.transform);
+				newSheep.GetComponent<StateCartridgeController> ().state = sd.sheepstate;
+			}
+		}
+	}
+
+	public void LoadListBuildingData(List<BuildingData> buildingdatalist, GameObject prefab){
+		GameObject buildingList = GameObject.FindGameObjectWithTag ("BuildingList");
+		if(buildingList != null && buildingdatalist != null) {
+			foreach(BuildingData bd in buildingdatalist){
+				GameObject buildingOBJ = Instantiate (prefab, bd.getPositionVector3(), bd.getRotationQuaternion(), buildingList.transform);
+				buildingOBJ.GetComponent<Building> ().m_health.val = bd.health;
+			}
+		}
+	}
+
+	public void ClearCombatSceneData(GameObject sheepOBJHolder, GameObject buildingOBJHolder) {
+		ClearDataFromList (sheepOBJHolder);
+		ClearDataFromList (buildingOBJHolder);
+	}
 	//PauseScene,ResumeScene
 }
 
@@ -28,4 +110,18 @@ public abstract class LevelData {
 	//Generic Level Data not including the scene specific things.
 	public string sceneName;
 	public float score;
+}
+
+[Serializable]
+public class CombatLevelData : LevelData {
+	public PlayerData playerdata;
+	public List<SheepData> sheepdata;
+	public List<BuildingData> buildingdata;
+
+}
+[Serializable]
+public class BonusLevelData : LevelData {
+	public PlayerData playerdata;
+	public List<SheepData> sheepdata;
+
 }
